088f6e9637ee8952829a9b2bfd4d6d1f8426abdf
catiejojo <catiejo.pidel@gmail.com>
2016-05-09 16:12:29 -0500

    sanity checking...it's a hardware problem.

--- take_two/take_two.ino
+++ take_two/take_two.ino
@@ -13,12 +13,14 @@ int count2 = 0;
 int count3 = 0;
 int count4 = 0;
 
-int duration = 2;
+int duration = 1;
 int beat = 500;
 int granularity = 10;
-  
+
+bool debug = false;
+
 void setup() {
-//  Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
@@ -28,32 +30,49 @@ void setup() {
   pinMode(button2, INPUT);
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
+
+  digitalWrite(button1, LOW);
+  digitalWrite(button2, LOW);
+  digitalWrite(button3, LOW);
+  digitalWrite(button4, LOW);
 }
 
 void loop() {
+  if (debug == true) {
+    debugPins(2);
+  } else {
+    debugPins(1);
+  }
+  debug = !debug;
+  Serial.print("count is: ");
+  Serial.print(count1);
+  Serial.print("\n");
   if (count1 != 0) {
     digitalWrite(led1, HIGH);
+    Serial.print("LED should be on");
     count1--;
   }
-  pollForPresses();
+  delay(beat);
+//  pollForPresses();
   digitalWrite(led1, LOW);
+  Serial.print("LED should be off");
   if (count2 != 0) {
     digitalWrite(led2, HIGH);
     count2--;
   }
-  pollForPresses();
+//  pollForPresses();
   digitalWrite(led2, LOW);
   if (count3 != 0) {
     digitalWrite(led3, HIGH);
     count3--;
   }
-  pollForPresses();
+//  pollForPresses();
   digitalWrite(led3, LOW);
   if (count4 != 0) {
     digitalWrite(led4, HIGH);
     count4--;
   }
-  pollForPresses();
+//  pollForPresses();
   digitalWrite(led4, LOW);
 }
 
@@ -65,21 +84,42 @@ void pollForPresses() {
 }
 
 void checkPins() {
-  if (digitalRead(button1) == LOW) {
-//    Serial.print("button 1 is pressed\n");
+  if (digitalRead(button1) == HIGH) {
+    Serial.print("button 1 is pressed\n");
     count1 = duration;
+  } else {
+    Serial.print("button 1 is NOT pressed\n");
   }
-  if (digitalRead(button2) == LOW) {
+  if (digitalRead(button2) == HIGH) {
 //    Serial.print("button 2 is pressed\n");
     count2 = duration;
   }
-  if (digitalRead(button3) == LOW) {
+  if (digitalRead(button3) == HIGH) {
 //    Serial.print("button 3 is pressed\n");
     count3 = duration;
   }
-  if (digitalRead(button4) == LOW) {
+  if (digitalRead(button4) == HIGH) {
 //    Serial.print("button 4 is pressed\n");
     count4 = duration;
   }
+}
+
+void debugPins(int seq) {
+  if (seq == 0) {
+    //do nothing
+  } else if (seq == 1) {
+    digitalWrite(button1, HIGH);
+    digitalWrite(button2, HIGH);
+    digitalWrite(button3, HIGH);
+    digitalWrite(button4, HIGH);
+    checkPins();
+  } else if (seq == 2) {
+    digitalWrite(button1, LOW);
+    digitalWrite(button2, LOW);
+    digitalWrite(button3, LOW);
+    digitalWrite(button4, LOW);
+    checkPins();
+  }
+  delay(beat);
 }
 
